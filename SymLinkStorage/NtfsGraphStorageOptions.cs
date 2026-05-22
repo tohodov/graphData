84f38cb6af95482d79ffd047f6bf78460e5a3bc1
a <@>
2026-05-22 10:32:29 +0500

    пути

--- SymLinkStorage/NtfsGraphStorageOptions.cs
+++ SymLinkStorage/NtfsGraphStorageOptions.cs
@@ -2,7 +2,7 @@ namespace SymLinkStorage;
 
 public sealed class NtfsGraphStorageOptions
 {
-    public string RootPath { get; init; } = Path.Combine(AppContext.BaseDirectory, "graph-data");
+    public string RootPath { get; set; } = Path.Combine(AppContext.BaseDirectory, "graph-data");
 
     public string MetadataFileName { get; init; } = "node.json";
 }
