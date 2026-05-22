66e47bf6647c90775d6c1bae93625e497f37c2da
a <@>
2026-05-22 10:38:08 +0500

    лишнее

--- Core/Extensions/ServiceCollectionExtensions.cs
+++ Core/Extensions/ServiceCollectionExtensions.cs
@@ -8,7 +8,6 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddGraphCore(this IServiceCollection services)
     {
         services.AddScoped<NodeService, NodeService>();
-        services.AddScoped<IncrementalGraphExpansionService, IncrementalGraphExpansionService>();
         services.AddScoped<GraphSearchService, GraphSearchService>();
         return services;
     }
